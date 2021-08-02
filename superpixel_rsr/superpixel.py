481cda9855f1a83889b476dc508b6b6460fa56e6
Sam Christian <50682137+Sam-2727@users.noreply.github.com>
2021-08-02 14:15:26 -0500

    Update superpixel.py

--- superpixel_rsr/superpixel.py
+++ superpixel_rsr/superpixel.py
@@ -8,15 +8,24 @@ import rsr
 import alphashape
 from shapely.geometry import Point
 from shapely import vectorized
+from joblib import Parallel, delayed
 class SuperPixel():
     def __init__(self,lon,lat,surf_amp):
         self.lon = lon
         self.lat = lat
         #stored as log surfAmp
         self.surfAmp = surf_amp
-    '''
-    numlon and numlat are the number of grid points along the longitude and latitude directions, respectively
-    '''
+    """
+    Function to grid surface amplitude based on user specified spacing. Stores gridded longitude in xgrid,
+    gridded latitued in ygrid, and gridded surface amplitude in zgrid.
+
+    Parameters
+    ----------
+    numlon : int
+        number of grid points along longitude direction
+    numlat : int
+        number of grid points along latitude direction
+    """
     def gridAmp(self,numlon,numlat):
         # Interpolation (non-spherical, for now)
         xgrid, ygrid = np.meshgrid(np.linspace(np.min(self.lon), np.max(self.lon), numlon)\
@@ -28,6 +37,14 @@ class SuperPixel():
         self.xgrid = xgrid
         self.ygrid = ygrid
         self.zgrid = zgrid
+    """
+    Function to calculate superpixels over region. Results can be plotted with plotSuperpixel() function.
+
+    Parameters
+    ----------
+    nuSuperpixels : int
+        number of superpixels to use in calculation.
+    """        
     def calcSuperpixel(self,nSuperpixels):
         superpixel = boruvka_superpixel.BoruvkaSuperpixel()
         bosupix = boruvka_superpixel.BoruvkaSuperpixel()
@@ -40,33 +57,126 @@ class SuperPixel():
             out[out==uniqueRegions[i]]=i
         self.superpixels = out
     #aim for 10000 per superpixel
-    def in_hull(self,p, hull):
-        if not isinstance(hull,Delaunay):
-            hull = Delaunay(hull)
+    # def in_hull(self,p, hull):
+    #     if not isinstance(hull,Delaunay):
+    #         hull = Delaunay(hull)
 
-        return hull.find_simplex(p)>=0
-    def calcRSR(self):
+        # return hull.find_simplex(p)>=0
+    """
+    Function to apply RSR method over each superpixel. Warning: this is the most computationally intensive step.
+    superpixel grids can be accessed with <object name>.supixPointsReturn and points in each superpixel can be accessed with
+    <object name>.lonPointsReturn and <object name>.latPointsReturns
+
+    Parameters
+    ----------
+    parallelUnits : int
+        number of threads to create for parallel execution of code.
+    """         
+    def calcRSR(self,parallelUnits):
+        self.rsrReturns = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        self.supixPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        self.lonPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        self.latPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        _ = Parallel(parallelUnits)(delayed(self.calcRSRInternal)(i) for i in range(0,int(np.max(self.superpixels))+1))
+        return [np.vstack((self.lonPointsReturn,self.latPointsReturn)),self.rsrReturns]
+    """
+    Internal method for parallel execution of RSR on superpixels, shouldn't be called.
+    """           
+    def calcRSRInternal(self,i):
+        print(i)
         lonRavel = np.ravel(self.xgrid)
         latRavel = np.ravel(self.ygrid)
         supixRavel  = np.ravel(self.superpixels)
-        rsrReturns = []
-        for i in range(0,int(np.max(self.superpixels))+1):
-            supixCondition  = (supixRavel==i)
-            possiblePointsCondition = (self.lon<np.max(lonRavel[supixCondition]))&(self.lon>np.min(lonRavel[supixCondition]))\
-                &(self.lat<np.max(latRavel[supixCondition]))&(self.lat>np.min(latRavel[supixCondition]))
-            pointsPossible = np.vstack([self.lon[possiblePointsCondition],self.lat[possiblePointsCondition]]).T
-            alpha_shape = alphashape.alphashape(supixPoints, 100)
-            boolContains = vectorized.conatains(alpha_shape,pointsPossible[:,0],pointsPossible[:,1])
-            if(len(pointsPossible)/len(self.lon)>0.5):
-                print("skipped superpixel {0} -- too large".format(i))
-                continue
-            boolContains = vectorized.contains(alpha_shape,pointsPossible[:,0],pointsPossible[:,1])
-            supixPoints = np.vstack([lonRavel[supixCondition],latRavel[supixCondition]]).T
-            lenSurfAmp = len(np.array(self.surfAmp)[possiblePointsCondition][boolContains])
-            if(lenSurfAmp<100):
-                print("only {0} points in superpixel {1},skipping!".format(lenSurfAmp,i))
-                continue
-            rsrReturns.append(rsr.run.processor(self.surfAmp[possiblePointsCondition][boolContains], fit_model='hk'))
-            return rsrReturns
-    def plotSuperpixelsPower(self,nSuperpixels,xlim,ylim):
+        supixCondition  = (supixRavel==i)
+        possiblePointsCondition = (self.lon<np.max(lonRavel[supixCondition]))&(self.lon>np.min(lonRavel[supixCondition]))\
+            &(self.lat<np.max(latRavel[supixCondition]))&(self.lat>np.min(latRavel[supixCondition]))
+        pointsPossible = np.vstack([self.lon[possiblePointsCondition],self.lat[possiblePointsCondition]]).T
+        if(len(pointsPossible)/len(self.lon)>0.5):
+            print("skipped superpixel {0} -- too large".format(i))
+            return np.nan
+        supixPoints = np.vstack([lonRavel[supixCondition],latRavel[supixCondition]]).T
+        alpha_shape = alphashape.alphashape(supixPoints, 100)
+        boolContains = vectorized.contains(alpha_shape,pointsPossible[:,0],pointsPossible[:,1])
+        self.supixPointsReturn[i] = supixPoints
+        lenSurfAmp = len(np.array(self.surfAmp)[possiblePointsCondition][boolContains])
+        Parallel()
+        if(lenSurfAmp<100):
+            print("only {0} points in superpixel {1},skipping!".format(lenSurfAmp,i))
+            return np.nan
+        self.rsrReturns[i] = (rsr.run.processor(self.surfAmp[possiblePointsCondition][boolContains], fit_model='hk'))
+        self.lonPointsReturn[i] = self.lon[possiblePointsCondition][boolContains]
+        self.latPointsReturn[i] = self.lat[possiblePointsCondition][boolContains]
+    """
+    Plot superpixels over region (region is gridded power amplitude).
+
+    Parameters
+    ----------
+    nSuperpixels : int
+        Number of superpixels to plot.
+    xlim: length-2 list
+        xlimit of plot, optional. Ex: [0,10]
+    ylim: length-2 list
+        ylimit of plot, optional. Ex: [0,10]        
+    """           
+    def plotSuperpixelsPower(self,nSuperpixels,xlim=False,ylim=False):
         plt.imshow(self.supixObject.average(nSuperpixels, 1, self.zgrid),origin="lower")
+        if(xlim):
+            plt.xlim(xlim)
+        if(ylim):
+            plt.ylim(ylim)
+    """
+    Plot results of applying RSR method over superpixels onto grid.
+
+    Parameters
+    ----------
+    observable: str
+        RSR parameter to plot (ex: "pc-pn")
+    size : int
+        size of points that are plotted, optional.
+    xlim: length-2 list
+        xlimit of plot, optional. Ex: [0,10]
+    ylim: length-2 list
+        ylimit of plot, optional. Ex: [0,10] 
+    """            
+    def resultPlotGrid(self,observable,size=False,xlim=False,ylim=False):
+        plotx = np.array(0)
+        ploty = np.array(0)
+        plotz = np.array(0)
+        for i in range(0,len(self.supixPointsReturn)):
+            if(type(self.supixPointsReturn[i])!=int):
+                plotx = np.concatenate((plotx, self.supixPointsReturn[i][:,0]),axis=None)
+                ploty = np.concatenate((ploty, self.supixPointsReturn[i][:,1]),axis=None)    
+                plotz = np.concatenate((plotz,np.repeat(self.rsrReturns[i].power()[observable],len(self.supixPointsReturn[i][:,0]))),axis=None)
+        plotx = plotx[1:]
+        ploty = ploty[1:]
+        plotz = plotz[1:]
+        if(size):
+            plot = plt.scatter(plotx,ploty,c=plotz,s=size)
+        else:
+            plot = plt.scatter(plotx,ploty,c=plotz)
+        plt.colorbar(plot)
+        if(xlim):
+            plt.xlim(xlim)
+        if(ylim):
+            plt.ylim(ylim)
+    def resultPlotData(self,observable,size=False,xlim=False,ylim=False):
+        plotx = np.array(0)
+        ploty = np.array(0)
+        plotz = np.array(0)
+        for i in range(0,len(self.supixPointsReturn)):
+            if(type(self.supixPointsReturn[i])!=int):
+                plotx = np.concatenate((plotx, self.lonPointsReturn[i]),axis=None)
+                ploty = np.concatenate((ploty, self.latPointsReturn[i]),axis=None)    
+                plotz = np.concatenate((plotz,np.repeat(self.rsrReturns[i].power()[observable],len(self.lonPointsReturn[i]))),axis=None)
+        plotx = plotx[1:]
+        ploty = ploty[1:]
+        plotz = plotz[1:]
+        if(size):
+            plot = plt.scatter(plotx,ploty,c=plotz,s=size)
+        else:
+            plot = plt.scatter(plotx,ploty,c=plotz)
+        plt.colorbar(plot)
+        if(xlim):
+            plt.xlim(xlim)
+        if(ylim):
+            plt.ylim(ylim)            
