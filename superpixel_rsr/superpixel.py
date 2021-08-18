0808d209466c4cda6f61a2ff763d982c338d05db
Sam Christian <50682137+Sam-2727@users.noreply.github.com>
2021-08-18 14:51:27 -0500

    various changes (see description)
    * Adding memory conservation by storing results in files
    * Adding various parameters to plot function to make replotting faster if not changing underlying data

--- superpixel_rsr/superpixel.py
+++ superpixel_rsr/superpixel.py
@@ -9,12 +9,15 @@ import alphashape
 from shapely.geometry import Point
 from shapely import vectorized
 from joblib import Parallel, delayed
+import os
 class SuperPixel():
     def __init__(self,lon,lat,surf_amp):
         self.lon = lon
         self.lat = lat
         #stored as log surfAmp
         self.surfAmp = surf_amp
+        self.resultPlotGridLoaded = False
+        self.resultPlotDataLoaded = False
     """
     Function to grid surface amplitude based on user specified spacing. Stores gridded longitude in xgrid,
     gridded latitued in ygrid, and gridded surface amplitude in zgrid.
@@ -56,6 +59,7 @@ class SuperPixel():
         for i in range(0,nSuperpixels):
             out[out==uniqueRegions[i]]=i
         self.superpixels = out
+        self.nsupix = nSuperpixels
     #aim for 10000 per superpixel
     # def in_hull(self,p, hull):
     #     if not isinstance(hull,Delaunay):
@@ -79,13 +83,22 @@ class SuperPixel():
     
     Second component is the rsrReturns corresponding to each superpixel
     """         
-    def calcRSR(self,parallelUnits):
+    def calcRSR(self,filename="rsrResultObjects",startingSupix=0):
         self.rsrReturns = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
-        self.supixPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
-        self.lonPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
-        self.latPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
-        _ = Parallel(parallelUnits)(delayed(self.calcRSRInternal)(i) for i in range(0,int(np.max(self.superpixels))+1))
-        return [np.vstack((self.lonPointsReturn,self.latPointsReturn)),self.rsrReturns]
+        try:
+            os.mkdir(filename)
+        except:
+            print("Assuming that folder {0} exists in current directory".format(filename))
+            pass
+        self.filename = filename
+        # self.supixPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        # self.lonPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        # self.latPointsReturn = np.zeros(int(np.max(self.superpixels))+1,dtype=object)
+        self.totalCount = 0
+        for i in range(startingSupix,int(np.max(self.superpixels)+1)):
+            self.calcRSRInternal(i)
+        #_ = Parallel(parallelUnits)(delayed(self.calcRSRInternal)(i) for i in range(0,int(np.max(self.superpixels))+1))
+        return None
     """
     Internal method for parallel execution of RSR on superpixels, shouldn't be called.
     """           
@@ -104,15 +117,20 @@ class SuperPixel():
         supixPoints = np.vstack([lonRavel[supixCondition],latRavel[supixCondition]]).T
         alpha_shape = alphashape.alphashape(supixPoints, 100)
         boolContains = vectorized.contains(alpha_shape,pointsPossible[:,0],pointsPossible[:,1])
-        self.supixPointsReturn[i] = supixPoints
+        #self.supixPointsReturn[i] = supixPoints
+        np.save(self.filename + "/supixPoints{0}".format(i),supixPoints)
         lenSurfAmp = len(np.array(self.surfAmp)[possiblePointsCondition][boolContains])
-        Parallel()
         if(lenSurfAmp<100):
             print("only {0} points in superpixel {1},skipping!".format(lenSurfAmp,i))
             return np.nan
-        self.rsrReturns[i] = (rsr.run.processor(self.surfAmp[possiblePointsCondition][boolContains], fit_model='hk'))
-        self.lonPointsReturn[i] = np.array(self.lon[possiblePointsCondition][boolContains])
-        self.latPointsReturn[i] = np.array(self.lat[possiblePointsCondition][boolContains])
+        np.save(self.filename + "/rsrReturns{0}".format(i),(rsr.run.processor(self.surfAmp[possiblePointsCondition][boolContains], fit_model='hk')))
+        np.save(self.filename + "/lonPointsReturn{0}".format(i),np.array(self.lon[possiblePointsCondition][boolContains]))
+        np.save(self.filename + "/latPointsReturn{0}".format(i),np.array(self.lat[possiblePointsCondition][boolContains]))
+        self.totalCount+=1
+        return None
+        #self.rsrReturns[i] = 
+        #self.lonPointsReturn[i] = 
+        #self.latPointsReturn[i] = 
     """
     Plot superpixels over region (region is gridded power amplitude).
 
@@ -143,47 +161,101 @@ class SuperPixel():
     xlim: length-2 list
         xlimit of plot, optional. Ex: [0,10]
     ylim: length-2 list
-        ylimit of plot, optional. Ex: [0,10] 
+        ylimit of plot, optional. Ex: [0,10]
+    forceReload:
+        results are loaded and stored so replotting is faster, 
+        but the data can be reloaded if data is changed (or plotting from a different folder)
+        by setting this to True (default False), optional.
+    filename:
+        folder to load data from (default is folder written to by 
+        most recent calcRSR call), optional.
     """            
-    def resultPlotGrid(self,observable,size=False,xlim=False,ylim=False):
-        plotx = np.array(0)
-        ploty = np.array(0)
-        plotz = np.array(0)
-        for i in range(0,len(self.supixPointsReturn)):
-            if(type(self.supixPointsReturn[i])!=int):
-                plotx = np.concatenate((plotx, self.supixPointsReturn[i][:,0]),axis=None)
-                ploty = np.concatenate((ploty, self.supixPointsReturn[i][:,1]),axis=None)    
-                plotz = np.concatenate((plotz,np.repeat(self.rsrReturns[i].power()[observable],len(self.supixPointsReturn[i][:,0]))),axis=None)
-        plotx = plotx[1:]
-        ploty = ploty[1:]
-        plotz = plotz[1:]
+    def resultPlotGrid(self,observable,size=False,xlim=False,ylim=False,forceReload=False,filename=False):
+        if(filename):
+            self.filename = filename
+        if((not self.resultPlotGridLoaded ) or forceReload):
+            plotx = np.array(0)
+            ploty = np.array(0)
+            plotLoad = np.array(0)
+            totalCount = 0
+            for i in range(1,self.nsupix):
+                try:
+                    supixPointsReturn = np.load(self.filename + "/supixPoints{0}.npy".format(i),allow_pickle=True)
+                    rsrReturns = np.load(self.filename + "/rsrReturns{0}.npy".format(i),allow_pickle=True).item()
+                    plotx = np.concatenate((plotx, supixPointsReturn[:,0]),axis=None)
+                    ploty = np.concatenate((ploty, supixPointsReturn[:,1]),axis=None)    
+                    plotLoad = np.concatenate((plotLoad,np.repeat(rsrReturns.power(),len(supixPointsReturn[:,0]))),axis=None)
+                except(FileNotFoundError):
+                    print("Following superpixel index doesn't exist (likely doesn't contain enough points -- consider lowering number of superpixels?):", i)
+            plotx = plotx[1:]
+            ploty = ploty[1:]
+            plotLoad = plotLoad[1:]
+            self.gridPlot = plotLoad
+            self.gridX = plotx
+            self.gridY = ploty
+
+        plotz = []
+        for i in self.gridPlot:
+            plotz.append(i[observable])
         if(size):
-            plot = plt.scatter(plotx,ploty,c=plotz,s=size)
+            plot = plt.scatter(self.gridX,self.gridY,c=plotz,s=size)
         else:
-            plot = plt.scatter(plotx,ploty,c=plotz)
+            plot = plt.scatter(self.gridX,self.gridY,c=plotz)
         plt.colorbar(plot)
         if(xlim):
             plt.xlim(xlim)
         if(ylim):
             plt.ylim(ylim)
-    def resultPlotData(self,observable,size=False,xlim=False,ylim=False):
-        plotx = np.array(0)
-        ploty = np.array(0)
-        plotz = np.array(0)
-        for i in range(0,len(self.supixPointsReturn)):
-            if(type(self.supixPointsReturn[i])!=int):
-                plotx = np.concatenate((plotx, self.lonPointsReturn[i]),axis=None)
-                ploty = np.concatenate((ploty, self.latPointsReturn[i]),axis=None)    
-                plotz = np.concatenate((plotz,np.repeat(self.rsrReturns[i].power()[observable],len(self.lonPointsReturn[i]))),axis=None)
-        plotx = plotx[1:]
-        ploty = ploty[1:]
-        plotz = plotz[1:]
+    """
+    Clears data used for making of grid plot. Useful if you want 
+    to clear up memory and don't need to replot grid soon.
+    """        
+    def clearGrid(self):
+        self.gridX = None
+        self.gridY = None
+        self.gridPlot = None
+    def resultPlotData(self,observable,size=False,xlim=False,ylim=False,forceReload=False,filename=False):
+        if(filename):
+            self.filename = filename
+        if((not self.resultPlotDataLoaded) or forceReload):
+            plotx = np.array(0)
+            ploty = np.array(0)
+            plotLoad = np.array(0)
+            for i in range(1,self.nsupix):
+                try:
+                    supixPointsReturn = np.load(self.filename + "/supixPoints{0}.npy".format(i),allow_pickle=True)
+                    lonPointsReturn = np.load(self.filename + "/lonPointsReturn{0}.npy".format(i),allow_pickle=True)
+                    latPointsReturn = np.load(self.filename + "/latPointsReturn{0}.npy".format(i),allow_pickle=True)
+                    rsrReturns = rsrReturns = np.load(self.filename + "/rsrReturns{0}.npy".format(i),allow_pickle=True).item()
+                    plotx = np.concatenate((plotx, lonPointsReturn),axis=None)
+                    ploty = np.concatenate((ploty, latPointsReturn),axis=None)    
+                    plotLoad = np.concatenate((plotLoad,np.repeat(rsrReturns.power(),len(lonPointsReturn))),axis=None)
+                except(FileNotFoundError):
+                    print("Following superpixel index doesn't exist (likely doesn't contain enough points -- consider lowering number of superpixels?):", i)
+            plotx = plotx[1:]
+            ploty = ploty[1:]
+            plotLoad = plotLoad[1:]
+            self.dataPlot = plotLoad
+            self.dataX = plotx
+            self.dataY = ploty
+        plotz = []
+        for i in self.dataPlot:
+            plotz.append(i[observable])
+
         if(size):
-            plot = plt.scatter(plotx,ploty,c=plotz,s=size)
+            plot = plt.scatter(self.dataX,self.dataY,c=plotz,s=size)
         else:
-            plot = plt.scatter(plotx,ploty,c=plotz)
+            plot = plt.scatter(self.dataX,self.dataY,c=plotz,s=1)
         plt.colorbar(plot)
         if(xlim):
             plt.xlim(xlim)
         if(ylim):
-            plt.ylim(ylim)            
+            plt.ylim(ylim)
+    """
+    Clears data used for making of data plot. Useful if you want 
+    to clear up memory and don't need to replot data soon.
+    """ 
+    def clearData(self):
+        self.dataX = None
+        self.dataY = None
+        self.dataPlot = None
